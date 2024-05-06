c9788161702aba32cf699087c0d8289010e88079
Akash Garkate <156793232+akashgarkate@users.noreply.github.com>
2024-05-06 19:22:35 +0530

    Time: 2650 ms (25.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+/*Write your MySQL query statement below
+condition = sum(weight_limit)<=1000
+output = last person_name*/
+SELECT 
+    q1.person_name
+FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1;
